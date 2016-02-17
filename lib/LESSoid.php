9478c892690d7f8419002ca0d09ce69e279eeea1
Cameron Chunn <cchunn@curse.com>
2016-02-17 17:17:00 -0600

    Less_Version axed

--- lib/LESSoid.php
+++ lib/LESSoid.php
@@ -701,7 +701,7 @@ class Less_Cache{
 	private static function CompiledName( $files, $extrahash ){
 
 		//save the file list
-		$temp = array(Less_Version::cache_version);
+		$temp = array(170);
 		foreach($files as $file){
 			$temp[] = filemtime($file)."\t".filesize($file)."\t".$file;
 		}
@@ -819,16 +819,3 @@ class Less_Cache{
 
 }
 
-/**
- * Release numbers
- *
- * @package Less
- * @subpackage version
- */
-class Less_Version{
-
-	const version = '1.7.0.10';			// The current build number of less.php
-	const less_version = '1.7.0';		// The less.js version that this build should be compatible with
-	const cache_version = '170';		// The parser cache version
-
-}
